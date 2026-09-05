dd2dbe508a0d6797800b6d5c8117ba0712e651ca
LUONG VAN TRUONG <truongluong14092006@gmail.com>
2026-09-05 11:47:58 +0700

    Lvtlesson05Views

---
+++ Lvtlesson05Views/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+using Lvtlesson05Views.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Lvtlesson05Views.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        //Lập trình trên view với Razor Code
+        public IActionResult RazorCode()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
